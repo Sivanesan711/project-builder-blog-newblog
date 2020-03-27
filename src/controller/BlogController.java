53df25636cef4890798455eeacad7b7e8222cdd8
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-03-27 17:35:34 +0530

    Add files via upload

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -1,7 +1,9 @@
 package controller;
 
 import java.io.IOException;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,7 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-
+import model.Blog;
+import model.User;
 
 @WebServlet(urlPatterns= {"/blog"})
 public class BlogController extends HttpServlet {
@@ -32,19 +35,25 @@ public class BlogController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		String blogDetails = request.getParameter("selectedAnswers");
-		
-		
-		
-
-		
+	String blogDetails = request.getParameter("selectedAnswers");
+	String[] blogDetail = blogDetails.split(",");
+	
+	Blog blog = new Blog(blogDetail[0], blogDetail[1], null);
+	User user = new User(blogDetail[0], blogDetail[1], null);
+	
+	blog.setTitle("THE BLOG");
+	blog.setDescription("THIS IS MY SAMPLE BLOG");
+	LocalDateTime dt = LocalDateTime.now();
+	LocalDate d = null;
+	blog.setPostedOn(d);
+	
 		if(blog!=null) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
-	*/	
+	
 	}
 
 }
